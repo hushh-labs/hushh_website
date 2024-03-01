0ace9caf4b32fb51bea26a817b707051d617c6dc
Neelesh <damrianeelesh@gmail.com>
2024-03-01 18:10:24 +0530

    alignment fix of technology card

--- src/app/_components/primitives/serviceCard.jsx
+++ src/app/_components/primitives/serviceCard.jsx
@@ -6,8 +6,8 @@ const ServiceCard = ({ icon, title, description, alignItems, additionalProps  })
     <GridItem width={{ base: "100%", md: "100%" }} px={{base:'1rem'}} >
       <VStack display={'flex'} alignItems={{ md: alignItems || 'flex-start', base:'center'}} gap={'1rem'} {...additionalProps}>
         {icon}
-        <Heading textAlign={{base:'center'}} fontSize={{ md:'1.5rem', base:'1rem'}} fontWeight={700} color={'white'}>{title}</Heading>
-        <Text textAlign={{base:'center'}} fontWeight={500} lineHeight={{ md:'25px', base:'15px'}} color={'#ABABAB'} fontSize={{ md:'1rem', base:'0.75rem'}}>{description}</Text>
+        <Heading textAlign={{base:'center', md:'left'}} fontSize={{ md:'1.5rem', base:'1rem'}} fontWeight={700} color={'white'}>{title}</Heading>
+        <Text textAlign={{base:'center',md:'left'}} fontWeight={500} lineHeight={{ md:'25px', base:'15px'}} color={'#ABABAB'} fontSize={{ md:'1rem', base:'0.75rem'}}>{description}</Text>
       </VStack>
     </GridItem>
   );
